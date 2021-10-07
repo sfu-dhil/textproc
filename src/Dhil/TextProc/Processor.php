c3a14ec97465b70c23c63732e77087c91e6ab1f4
sabaa <saba_akhyani@sfu.ca>
2021-10-07 15:50:41 -0700

    Adding functions for processing text and testing

--- src/Dhil/TextProc/Processor.php
+++ src/Dhil/TextProc/Processor.php
@@ -13,7 +13,7 @@ namespace Dhil\TextProc;
 use Psr\Log\LoggerAwareInterface;
 use Psr\Log\LoggerAwareTrait;
 use Psr\Log\NullLogger;
-
+use Normalizer;
 /**
  * Implementation of some text processing functions.
  */
@@ -35,4 +35,49 @@ class Processor implements LoggerAwareInterface {
 
         return $a;
     }
+
+    /**
+     * @param string $a
+     * @return string
+     */
+    public function clean(string $a) : string {
+
+        $a= Normalizer::normalize($a,Normalizer::FORM_D);
+        $a= preg_replace('/\p{M}/u','',$a);
+
+        //remove spaces beginning and end
+        $a= preg_replace('/^\s+|\s+$/u','',$a);
+
+        //remove punctuation
+        $a= preg_replace('/\p{P}/u','',$a);
+
+        //to lowerCase
+        $a= mb_strtolower($a);
+
+        //multiSpace to singleSpace
+        return preg_replace('/\s+/u', ' ', $a);
+    }
+
+    public function countCharacters(string $a, bool $dirtFlag) : int {
+        //This function gets a string and count the characters inside, if the bool is true
+        if($dirtFlag)
+            return mb_strlen($a);
+    }
+    public function countLines(string $a, bool $dirtFlag) : int {
+        //This function gets a string, count the lines inside, if the bool is true
+        if($dirtFlag){
+            $matches=[];
+            preg_match_all('/(\r\n|\r|\n)/u', $a, $matches);
+            return count($matches[0]);
+        }
+    }
+    public function countWords(string $a, bool $dirtFlag) : int {
+        //This function gets a string, count the words inside, if the bool is true
+        if ($dirtFlag){
+            $matches=[];
+            preg_match_all('/[\pL\pN\pPd]+/u', $a, $matches);
+            return count($matches[0]);
+        }
+    }
+
 }
--- test/Dhil/TextProc/ProcessorTest.php
+++ test/Dhil/TextProc/ProcessorTest.php
@@ -15,11 +15,84 @@ use PHPUnit\Framework\TestCase;
 class ProcessorTest extends TestCase {
     private Processor $processor;
 
+
     /**
      * @test
+     * @dataProvider cleanData
      */
-    public function doStuff() : void {
-        $this->assertSame('a', $this->processor->doStuff('a'));
+    public function clean($expectedResult, $input) : void {
+          $this->assertSame($expectedResult, $this->processor->clean($input));
+    }
+    public function cleanData(){
+    return [
+        ['a','  a'],
+        ['',''],
+        ['','  '],
+        ['a','a  '],
+        ['a','a?'],
+        ['a','A'],
+        ['hello it is me','hello  it is me'],
+        ['aبنفش','aبنفش'],
+        ['e','é'],
+        ['e',"e\u{0301}",true], //é
+    ];
+    }
+    /**
+     * @test
+     * @dataProvider countCharactersData
+     */
+    public function countCharacters($expectedResult, $input, $dirtFlag): void {
+        $this->assertSame($expectedResult, $this->processor->countCharacters($input,$dirtFlag));
+    }
+    public function countCharactersData(){
+        return [
+            [0,'', true],
+            [1,'a',true],
+            [2,'a?',true],
+            [9,'hello hyd',true],
+            [4,'联合声明',true],
+            [4,'بنفش',true],
+            [1,'é',true],
+            [2,"e\u{0301}",true], //é  -> at end, we want it to be one (change line 60 processor.php)
+
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider countLinesData
+     */
+    public function countLines($expectedResult, $input, $dirtFlag): void{
+        $this->assertSame($expectedResult, $this->processor->countLines($input,$dirtFlag));
+    }
+    public function countLinesData(){
+        return [
+            [0,'', true],
+            [1,"\n",true],
+            [2,"hello\nhny\n",true],
+            [2,"hello\n\n",true],
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider countWordsData
+     */
+    public function countWords($expectedResult, $input, $dirtFlag): void{
+        $this->assertSame($expectedResult, $this->processor->countWords($input,$dirtFlag));
+
+    }
+    public function countWordsData(){
+        return [
+            [4,'azizam بگو bar migardi', true],
+            [1,'woo-hoo',true],
+            [1,'aziz?i',true],
+            [0,'',true],
+            [3,'azizami ؟ مگه',true],
+            [1,'联合声明',true],
+            [2,'hello  what',true],
+
+        ];
     }
 
     protected function setUp() : void {
